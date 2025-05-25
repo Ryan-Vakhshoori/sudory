c015119d6941a51216cb80f569936158afa55bda
Ryan-Vakhshoori <96512123+Ryan-Vakhshoori@users.noreply.github.com>
2025-05-24 21:36:57 -0700

    fix: comment out method check in submit-stats API handler

--- src/pages/api/submit-stats.ts
+++ src/pages/api/submit-stats.ts
@@ -7,7 +7,7 @@ const supabase = createClient(
 );
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") return res.status(405).end();
+  // if (req.method !== "POST") return res.status(405).end();
 
   const { puzzleIndex, difficulty, time, moveCount } = req.body;
   const { error } = await supabase.from("completions").insert([
